979eeedffb22af03ac11072287f093b0798dc61f
sal rashid <salrashid123@gmail.com>
2026-02-24 13:47:30 -0500

    update aws test
    Signed-off-by: sal rashid <salrashid123@gmail.com>

--- awskms/awskms_test.go
+++ awskms/awskms_test.go
@@ -22,8 +22,9 @@ func TestKMSDSA65(t *testing.T) {
 	access_secret_id := os.Getenv("CICD_AWS_ACCESS_SECRET")
 	aws_region := os.Getenv("CICD_AWS_REGION")
 
-	os.Setenv("AWS_ACCESS_KEY", access_key_id)
-	os.Setenv("AWS_ACCESS_SECRET", access_secret_id)
+	os.Setenv("AWS_ACCESS_KEY_ID", access_key_id)
+	os.Setenv("AWS_SECRET_ACCESS_KEY", access_secret_id)
+	os.Setenv("AWS_REGION", aws_region)
 
 	keyID := "37aca4ea-3915-441f-b03d-d90bad1eb45a"
 	region := aws_region
